b27f8e14775c0132fc076378848426befc7d5953
teddsk <ted.dossou-koko@epitech.eu>
2025-03-18 21:27:34 +0100

    test_app

--- authentification_app/urls.py
+++ authentification_app/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from .views import signup, login_user, logout_user, update_user_details, list_utilisateurs, homepage
 
 urlpatterns = [
-    # path("", home_view, name="home"),
-    # path("signup/", signup_view, name="signup"),
-    # path("login/", login_view, name="login"),
-    # path("logout/", logout_view, name="logout"),
-
     path('', homepage, name='homepage'),
     path('api/signup/', signup, name='api_signup'),
     path('api/login/', login_user, name='api_login'),
